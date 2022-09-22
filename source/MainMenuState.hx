668306089f31eef543065d960b20fdce42696e8e
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-09-22 08:49:20 -0200

    Fds isso

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -74,10 +74,7 @@ class MainMenuState extends MusicBeatState
 		persistentUpdate = persistentDraw = true;
 
 		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
-		// bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('Main_Menu_Spritesheet_Animation'));//
-		var makeAnimatedLuaSprite('Main_Menu_Spritesheet_Animation', 'images/Main_Menu_Spritesheet_Animation', -0.25, -0.05;
-		var addAnimationByPrefix('Main_Menu_Spritesheet_Animation', 'Main_Menu_Spritesheet_Animation', 'Main_Menu_Spritesheet_Animation', 24, true)',
-		var objectPlayAnimation('Main_Menu_Spritesheet_Animation', 'Main_Menu_Spritesheet_Animation', true);
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('MainBG'));
 		bg.scrollFactor.set(0, yScroll);
 		bg.setGraphicSize(Std.int(bg.width * 1.175));
 		bg.updateHitbox();
