ede3c3ea9afd918f5be6166f000ac4d498fd19df
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-09-22 12:47:42 -0200

    Update Sanic.lua

--- assets/preload/stages/Sanic.lua
+++ assets/preload/stages/Sanic.lua
@@ -1,7 +1,7 @@
 function onCreate()
 	
 	makeLuaSprite('sanicbg', 'exe/sanicbg', -550, -115);
-	addLuaSprite('sanicbg', true);
+	addLuaSprite('sanicbg', false);
     scaleObject('sanicbg', 1.3, 1.2)  
 
    setProperty('defaultCamZoom', 0.75) 
